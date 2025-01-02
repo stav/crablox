0a5425ae7b6cce68285203b125e063adc53377b8
Steven Almeroth <steven.almeroth@gmail.com>
2025-01-02 00:10:26 -0500

    cleanup

--- crablox/main.py
+++ crablox/main.py
@@ -3,9 +3,9 @@ from fh_altair import altair_headers
 
 from style import styles
 from hauls import (
-    hello_block,
-    example_block,
-    tradesties_block,
+    # hello_block,
+    # example_block,
+    # tradesties_block,
     um_index_block,
     um_prices_block,
     um_components_block,
@@ -20,13 +20,13 @@ def index():
     return (
         Titled(
             "Say Hello to Crablox",
-            hello_block(rt),
-            example_block(rt),
-            tradesties_block(rt),
+            # hello_block(rt),
+            # example_block(rt),
+            # tradesties_block(rt),
+            ism_mfg_block(rt),
             um_index_block(rt),
             um_components_block(rt),
             um_prices_block(rt),
-            ism_mfg_block(rt),
         ),
     )
 
