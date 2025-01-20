7f51f12b2272cc8d400de9c678a96e8a1ec0a965
Steven Almeroth <steven.almeroth@gmail.com>
2025-01-19 19:32:07 -0500

    update bld permits

--- crablox/hauls/h_bld_permits.py
+++ crablox/hauls/h_bld_permits.py
@@ -13,13 +13,30 @@ def content():
     details = Card(
         H3("United States Building Permits"),
         H5("🏗️ Housing Starts: Analysts Called It Wrong"),
-        A(
+        P(
+            """
+            1.499M units, smashing expectations of 1.32M. Biggest print since March 2021.
+            The real story is in the details. Multi-family units exploded 58.9% to 418k while single-family barely moved (+3.3%).
+            Northeast is absolutely ripping at +40.2% while the West actually cooled off (-0.7%), the Midwest (20% to 204,000), and the South (17.7% to 853,000).
+            One the surface, it may look like commercial real estate players are betting big on multi-family. The reality is those guys just want to lock in rates now in fear rates will stay higher for longer or possibly go back up.
+            While single-family construction won't budge for the most part until Powell lowers rates more.
+            """
+        ),
+        P(
             Img(
-                src="/static/NewResidentialConstruction.png",
-                alt="New Residential Construction Chart",
+                src="/static/US_Building_Permits-vs-Housing_starts.png",
+                alt="Building permits Vs Housing starts Chart",
             ),
-            href="https://www.census.gov/construction/nrc/pdf/newresconst.pdf",
-            target="_blank",
+        ),
+        P(
+            A(
+                Img(
+                    src="/static/NewResidentialConstruction.png",
+                    alt="New Residential Construction Chart",
+                ),
+                href=url1,
+                target="_blank",
+            ),
         ),
         cls="wlv-details",
         footer=Ul(
@@ -29,9 +46,13 @@ def content():
     )
     return Div(
         details,
-        Img(
-            src="/static/US_Building_Permits.png",
-            alt="United States Building Permits Chart",
+        A(
+            Img(
+                src="/static/US_Building_Permits.png",
+                alt="United States Building Permits Chart",
+            ),
+            href=url2,
+            target="_blank",
         ),
         id=id,
     )
