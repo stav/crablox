0a0faa37d94d88b322dd87b0cf987a95c16fa495
Steven Almeroth <steven.almeroth@gmail.com>
2025-02-19 22:28:35 -0500

    index module

--- crablox/main.py
+++ crablox/main.py
@@ -1,5 +1,3 @@
-from types import ModuleType
-
 from starlette.responses import RedirectResponse
 from starlette.requests import Request
 from fasthtml.common import (
@@ -9,21 +7,15 @@ from fasthtml.common import (
     Div,
     fast_app,
     Form,
-    Img,
     Input,
     Label,
     P,
-    ScriptX,
     serve,
     Titled,
-    Svg,
 )
-from fa6_icons import svgs
 
+import index
 from config import env, fast_config, USERNAME
-
-import hauls
-from blocks import stack
 
 
 def user_auth_before(req, sess):
@@ -48,80 +40,9 @@ app, rt = fast_app(before=beforeware, **fast_config)
 print(f'Using "{env}" environment for {app}')
 
 
-def create_route(block: ModuleType):
-    """
-    Creates a route for the given block.
-
-    This function is a wrapper around the `rt` decorator to create a scope for
-    the block object at runtime.
-
-    This wrapper creates a route using the block's path. This route is a GET
-    request handler that returns the contents of the block.
-
-    Args:
-        block: A module object representing a block with: path, id, title, content()
-
-    Returns:
-        None
-    """
-
-    @rt(block.path)
-    def get():
-        return block.content()
-
-
-def block_stacker():
-    """
-    Generator function that processes hauls and yields a container for each
-    block's: path, id, and title.
-
-    Yields:
-        The result of the stack function for each block.
-    """
-    for block in hauls.blocks:
-        create_route(block)
-        yield stack(block.path, block.id, block.title)
-
-
-@rt
-def index(req: Request):
-    path = "crablox/main.js"
-    return (
-        Titled(
-            "Indicator Megaboard Dashboard",
-            Div(
-                *block_stacker(),
-                A(
-                    Svg(
-                        svgs.arrow_right_from_bracket.solid,
-                    ),
-                    href="/logout",
-                    title="Logout",
-                    style="width: 22em; display: flex;",
-                ),
-                cls="swapy-container",
-            ),
-        ),
-        Div(
-            Div(Img(id="lightbox-img"), cls="lightbox-image"),
-            Div(id="lightbox-cap", cls="lightbox-caption"),
-            Div(id="lightbox-det", cls="lightbox-details"),
-            # X closes lightbox, also escape key listener in main.js
-            Div(
-                Button(
-                    "X",
-                    cls="crb-close",
-                    data_swapy_no_drag=True,
-                    title="Try me, or hit escape key",
-                ),
-                cls="lightbox-close",
-                onclick="closeLightbox()",
-            ),
-            cls="lightbox",
-            id="lightbox",
-        ),
-        ScriptX(path),
-    )
+@rt("/")
+def _(req: Request):
+    return index.route(rt)
 
 
 @rt("/logout")
