96ea5033a71148a344c02123f5c028698fbca1a1
Steven Almeroth <steven.almeroth@gmail.com>
2025-02-23 20:24:33 -0500

    update: bld permits

--- crablox/hauls/h_bld_permits.py
+++ crablox/hauls/h_bld_permits.py
@@ -1,4 +1,4 @@
-from fasthtml.common import A, Card, Div, H3, H5, Img, Li, P, Ul
+from fasthtml.common import A, Card, Div, Img, Li, P, Ul
 
 id = "BuildingPermits"
 path = "/building_permits"
@@ -11,18 +11,26 @@ def content():
     url1 = "https://www.census.gov/construction/nrc/pdf/newresconst.pdf"
     url2 = "https://tradingeconomics.com/united-states/building-permits"
 
-    details = Card(
-        H3("United States Building Permits"),
-        P("17 January 2025"),
-        H5("🏗️ Housing Starts: Analysts Called It Wrong"),
-        P(
+    details = (
+        Div(
             """
-            1.499M units, smashing expectations of 1.32M. Biggest print since March 2021.
-            The real story is in the details. Multi-family units exploded 58.9% to 418k while single-family barely moved (+3.3%).
-            Northeast is absolutely ripping at +40.2% while the West actually cooled off (-0.7%), the Midwest (20% to 204,000), and the South (17.7% to 853,000).
-            One the surface, it may look like commercial real estate players are betting big on multi-family. The reality is those guys just want to lock in rates now in fear rates will stay higher for longer or possibly go back up.
-            While single-family construction won't budge for the most part until Powell lowers rates more.
-            """
+### United States Building Permits
+
+17 January 2025
+
+##### 🏗️ Housing Starts: Analysts Called It Wrong
+
+1.499M units, smashing expectations of 1.32M. Biggest print since March 2021.
+
+The real story is in the details. Multi-family units exploded 58.9% to 418k while single-family barely moved (+3.3%).
+
+Northeast is absolutely ripping at +40.2% while the West actually cooled off (-0.7%), the Midwest (20% to 204,000), and the South (17.7% to 853,000).
+
+One the surface, it may look like commercial real estate players are betting big on multi-family. The reality is those guys just want to lock in rates now in fear rates will stay higher for longer or possibly go back up.
+
+While single-family construction won't budge for the most part until Powell lowers rates more.
+""",
+            cls="marked",
         ),
         P(
             Img(
@@ -44,12 +52,8 @@ def content():
             onclick="openLightbox(this)",
             data_id=id,
         ),
-        cls="wlv-details",
-        footer=Ul(
-            Li(A(url1, href=url1, target="_blank")),
-            Li(A(url2, href=url2, target="_blank")),
-        ),
     )
+
     return (
         Div(
             Img(
@@ -61,5 +65,12 @@ def content():
             onclick="openLightbox(this)",
             data_id=id,
         ),
-        details,
+        Card(
+            details,
+            cls="wlv-details",
+            footer=Ul(
+                Li(A(url1, href=url1, target="_blank")),
+                Li(A(url2, href=url2, target="_blank")),
+            ),
+        ),
     )
