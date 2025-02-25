334d5ec8c78b85c2021ed4bb2b983809c2e0d0d6
Steven Almeroth <steven.almeroth@gmail.com>
2025-02-25 01:30:08 -0500

    new bld permits

--- crablox/hauls/h_bld_permits.py
+++ crablox/hauls/h_bld_permits.py
@@ -16,7 +16,13 @@ def content():
             """
 ### United States Building Permits
 
-17 January 2025
+#### 19 February 2025
+
+Building permits in the United States inched higher by 0.1% to a seasonally adjusted annualized rate of 1.483 million in January of 2025, firmly above market expectations that they would fall to 1.46 million, according to a preliminary estimate. Permits for housing with two to four units surged by 13.2% from the earlier month to 60 thousand, offsetting the 1.4% drop in housing with five or more units (to 427 thousand), while permits for housing with one unit remained unchanged at 996 thousand. Among different regions, permits rose in the West (2.3% to 316 thousand) and the Midwest (1.8% to 223,000). These outweighed the marginal drop in the South (-0.1% to 806 thousand) and the sharper drop in the Northeast (-6.1% to 138 thousand).
+
+_source: U.S. Census Bureau_
+
+#### 17 January 2025
 
 ##### 🏗️ Housing Starts: Analysts Called It Wrong
 
