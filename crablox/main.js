7ae4a071e7658614ede4a6c6fa9967ac2d7bb4ef
Steven Almeroth <steven.almeroth@gmail.com>
2025-03-18 11:34:35 -0400

    js doc

--- crablox/main.js
+++ crablox/main.js
@@ -1,5 +1,17 @@
-// import htmx from 'htmx.org';
+/**
+ * @module main
+ * @description UI interactions and setup.
+ *
+ * Opening and closing blocks, managing a lightbox, and initializing the Swapy library.
+ */
 
+// BLOCKS
+
+/**
+ * Opens a data block and reveals the clear button within a specified item.
+ *
+ * @param {HTMLElement} openButton - The button element that triggers the opening of the data block.
+ */
 function crbOpenBlock (openButton) {
   const item = openButton.closest('.item')
   // Reveal data block
@@ -9,6 +21,12 @@ function crbOpenBlock (openButton) {
   const clearButton = item.querySelector('.wlv-clear')
   clearButton.style.display = 'inline-block'
 }
+
+/**
+ * Clears and hides a data block and its associated clear button.
+ *
+ * @param {HTMLElement} clearButton - The clear button element that triggers the clear action.
+ */
 function crbClearBlock (clearButton) {
   // Hide clear button
   clearButton.style.display = 'none'
@@ -17,12 +35,26 @@ function crbClearBlock (clearButton) {
   const data = item.querySelector(`.wlv-data`)
   data.style.display = 'none'
 }
+
+/**
+ * Closes and removes the closest parent element with the class 'slot' of the given close button.
+ *
+ * @param {HTMLElement} closeButton - The button element that triggers the close action.
+ */
 function crbCloseBlock (closeButton) {
   const slot = closeButton.closest('.slot')
   slot.outerHTML = ''
 }
 
-// https://www.bualabs.com/archives/5073/displaying-cloudinary-images-in-fasthtml-ep-3/
+// LIGHTBOX
+
+/**
+ * Opens a lightbox with the image, caption, and details from the provided wrapper element.
+ *
+ * https://www.bualabs.com/archives/5073/displaying-cloudinary-images-in-fasthtml-ep-3/
+ *
+ * @param {HTMLElement} wrapper - The wrapper element containing the image and details to display in the lightbox.
+ */
 function openLightbox (wrapper) {
   // Image
   const image = wrapper.querySelector('img')
@@ -36,9 +68,17 @@ function openLightbox (wrapper) {
   document.getElementById('lightbox-det').innerHTML = details.innerHTML
   document.getElementById('lightbox').style.display = 'flex'
 }
+
+/**
+ * Hides the lightbox element by setting its display style to 'none'.
+ */
 function closeLightbox () {
   document.getElementById('lightbox').style.display = 'none'
 }
+
+/**
+ * Listen for keyboard events: escape closes lightbox
+ */
 document.addEventListener('keydown', function (event) {
   console.log(event.key)
   if (event.key === 'Escape') {
@@ -46,14 +86,38 @@ document.addEventListener('keydown', function (event) {
   }
 })
 
-// Swapy
+// SWAPY
+
+/**
+ * The container to hold the #block-grid and all subsequent Swapy slots.
+ */
 const container = document.querySelector('.swapy-container')
-const swapy = Swapy.createSwapy(container, {
-  animation: 'dynamic'
-  // swapMode: 'drop',
-  // autoScrollOnDrag: true,
-  // enabled: true,
-  // dragAxis: 'x',
-  // dragOnHold: true
-})
-swapy.enable(true)
+
+/**
+ * @type {import("../vendor/swapy/swapy").Config}
+ */
+const options = {
+  animation: 'dynamic',
+  enabled: true
+}
+
+/**
+ * The global Swapy object is created by swapy.min.js via swapy.js.
+ *
+ * @typedef {Object} Swapy
+ * @property {function} createSwapy
+ * @property {function} getClosestScrollableContainer
+ * @property {Object} utils
+ *
+ * @global Swapy
+ * @type {Swapy}
+ */
+var Swapy
+
+/**
+ * The `createSwapy` function returns an object with a `Swapy` interface.
+ * This `Swapy` interface is different than the global `Swapy` object.
+ *
+ * @type {import("../vendor/swapy/swapy").Swapy}
+ */
+const swapy = Swapy.createSwapy(container, options)
