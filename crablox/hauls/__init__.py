d15b6e3cd8ebe5c78403fd7071cb266936ace6ea
Steven Almeroth <steven.almeroth@gmail.com>
2025-05-08 21:13:35 -0400

    rename jobs

--- crablox/hauls/__init__.py
+++ crablox/hauls/__init__.py
@@ -2,15 +2,15 @@ from typing import List, Any, Callable
 from types import ModuleType
 
 from . import (
-    h_bls_esr,
-    h_ism_mfg,
-    h_ism_srv,
     h_bld_permits,
     h_cmc_btc,
     h_gdp,
-    h_um_ics,
+    h_ism_mfg,
+    h_ism_srv,
+    h_jobs_payroll,
     h_lei_us,
     h_ticker,
+    h_um_ics,
 )
 
 
@@ -33,7 +33,7 @@ blocks: List[Haul] = [
     Haul(h_ism_srv),
     Haul(h_um_ics),
     Haul(h_bld_permits),
-    Haul(h_bls_esr),
+    Haul(h_jobs_payroll),
     Haul(h_gdp),
     Haul(h_lei_us),
     Haul(h_cmc_btc),
