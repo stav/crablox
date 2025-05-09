f0d239afe077b58718bc10dcf69888e886c6f12c
Steven Almeroth <steven.almeroth@gmail.com>
2025-05-08 21:17:30 -0400

    new format for jobs

--- crablox/hauls/h_jobs_payroll.py
+++ crablox/hauls/h_jobs_payroll.py
@@ -1,5 +1,9 @@
-from fasthtml.common import A, Card, Div, Img, Li, Ul
+from fasthtml.common import Card, Div, Img, P
 
+from .components import get_details, get_history, get_footer
+
+id = "JobsPayroll"
+path = "/jobs_payroll"
 title = "Empl Situation · 151 K"
 short = "ESR"
 style = "background-color: var(--pico-color-jade-500); border-color: var(--pico-color-jade-300);"
@@ -7,53 +11,15 @@ caption = "US Non-Farm Payrolls"
 summary = "Below forecasts, up from a downwardly revised 125K in January"
 
 
+def history():
+    return get_history(__file__)
+
+
+def details():
+    return get_details(__file__)
+
+
 def content():
-
-    url1 = "https://tradingeconomics.com/united-states/non-farm-payrolls"
-    url2 = "https://www.bls.gov/news.release/empsit.nr0.htm"
-
-    details = Div(
-        """
-# 151 K
-### Employment Situation Report
-United States Non-Farm Payrolls for February 2025
-
-#### 7 March 2025
-Total nonfarm payroll employment rose by 151,000 in February, and the unemployment rate changed
-little at 4.1 percent, the U.S. Bureau of Labor Statistics reported. Employment trended
-up in health care, financial activities, transportation and warehousing, and social assistance.
-Federal government employment declined.
-
-#### 7 February 2025
-
-Total nonfarm payroll employment rose by 143,000 in January, and the unemployment rate edged down to 4.0 percent, the U.S. Bureau of Labor Statistics reported today. Job gains occurred in health care, retail trade, and social assistance. Employment declined in the mining, quarrying, and oil and gas extraction industry.
-
-#### 10 January 2025
-Total nonfarm payroll employment increased by 256,000 in December, and the unemployment rate
-changed little at 4.1 percent, the U.S. Bureau of Labor Statistics reported. Employment
-trended up in health care, government, and social assistance. Retail trade added jobs in
-December, following a job loss in November.
-
-##### NonFarm Payrolls - Good but Bad
-NFP Dec: 256K vs 160K exp (Nov revised to 212K)
-
-- Healthcare +46K
-- Retail +43K
-- Gov't +33K
-- Manufacturing -13K
-
-##### Key points: Huge beat - not even close to estimates.
-
-Nov got revised down but doesn't matter, labor market's still cooking. Retail bounced back hard after that weak Nov.
-
-2024 avg monthly gains = 186K vs 251K in 2023.
-
-Bottom line: Labor market refuses to break. Add in today's hot Michigan inflation expectations and the Fed's got zero reason to cut.
-
-Rates staying higher for even longer.
-""",
-        cls="marked",
-    )
     return (
         Div(
             Img(
@@ -65,11 +31,21 @@ Rates staying higher for even longer.
             onclick="openLightbox(this)",
         ),
         Card(
-            details,
+            Div(details(), cls="marked"),
             cls="wlv-details",
-            footer=Ul(
-                Li(A(url1, href=url1, target="_blank")),
-                Li(A(url2, href=url2, target="_blank")),
+            header=caption,
+            footer=get_footer(
+                [
+                    (
+                        "Trading Economics: Non-Farm Payrolls",
+                        "https://tradingeconomics.com/united-states/non-farm-payrolls",
+                    ),
+                    (
+                        "BLS: Employment Situation Summary",
+                        "https://www.bls.gov/news.release/empsit.nr0.htm",
+                    ),
+                ],
+                history(),
             ),
         ),
     )
---
+++ crablox/hauls/h_jobs_payroll|.md
@@ -0,0 +1,37 @@
+# 151 K
+### Employment Situation Report
+United States Non-Farm Payrolls for February 2025
+
+#### 7 March 2025
+Total nonfarm payroll employment rose by 151,000 in February, and the unemployment rate changed
+little at 4.1 percent, the U.S. Bureau of Labor Statistics reported. Employment trended
+up in health care, financial activities, transportation and warehousing, and social assistance.
+Federal government employment declined.
+
+#### 7 February 2025
+
+Total nonfarm payroll employment rose by 143,000 in January, and the unemployment rate edged down to 4.0 percent, the U.S. Bureau of Labor Statistics reported today. Job gains occurred in health care, retail trade, and social assistance. Employment declined in the mining, quarrying, and oil and gas extraction industry.
+
+#### 10 January 2025
+Total nonfarm payroll employment increased by 256,000 in December, and the unemployment rate
+changed little at 4.1 percent, the U.S. Bureau of Labor Statistics reported. Employment
+trended up in health care, government, and social assistance. Retail trade added jobs in
+December, following a job loss in November.
+
+##### NonFarm Payrolls - Good but Bad
+NFP Dec: 256K vs 160K exp (Nov revised to 212K)
+
+- Healthcare +46K
+- Retail +43K
+- Gov't +33K
+- Manufacturing -13K
+
+##### Key points: Huge beat - not even close to estimates.
+
+Nov got revised down but doesn't matter, labor market's still cooking. Retail bounced back hard after that weak Nov.
+
+2024 avg monthly gains = 186K vs 251K in 2023.
+
+Bottom line: Labor market refuses to break. Add in today's hot Michigan inflation expectations and the Fed's got zero reason to cut.
+
+Rates staying higher for even longer. 
