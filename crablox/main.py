f0be59ab11b8f1eadfe9e27556cbb7b99f6ea0ff
Steven Almeroth <steven.almeroth@gmail.com>
2025-05-26 10:32:12 -0400

    routes

--- crablox/main.py
+++ crablox/main.py
@@ -1,79 +1,13 @@
-from starlette.requests import Request
-from fasthtml.common import fast_app, serve, Div
-from pathlib import Path
+from fasthtml.common import fast_app, serve
 
 import auth
-import audio
-import index
-import hauls
-import ticker
-from blocks import stack
+import routes
 from config import env, fast_config, reload
 
 app, rt = fast_app(before=auth.beforeware, **fast_config)
 # app.htmlkw["data-theme"] = "dark"
 print(f'Using "{env}" environment for {app}')
+
+routes.register_routes(app, rt)
+
 serve(reload=reload)
-
-# Routes
-#######################################################
-# /                 | index  | Home page
-# /login            | auth   | Login page & service
-# /logout           | auth   | Logout service
-# /audio/{id}       | audio  | Get audio file by id
-# /static/{file}    |        | Get static files by name
-# /api/blocks/{id}  | main   | Get block by id
-# /api/history/{f}  | main   | Get history markup
-# /api/lookup       | ticker | Lookup ticker data
-# /api/search       | ticker | Search ticker suggestions
-# /favicon.ico      | config | /static/favicon.ico
-
-
-@rt("/")
-def _():
-    return index.route(rt)
-
-
-@rt("/api/blocks/{id}")
-def _(id: str):
-    for block in hauls.blocks:
-        if block.id == id:
-            return stack(block)
-    return f"Block not found: {id}", 404
-
-
-@rt("/api/history/{parent}/{filename}")
-def _(parent: str, filename: str):
-    file_path = Path("crablox/hauls") / parent / filename
-    markup = file_path.read_text()
-    return Div(markup, cls="marked")
-
-
-@rt("/api/lookup")
-def _(request: Request):
-    return ticker.display(request)
-
-
-@rt("/api/search")
-def _(request: Request):
-    return ticker.search(request)
-
-
-@rt("/audio/{id}")
-def _(id: str):
-    return audio.get_audio_file(id)
-
-
-@app.get("/login")
-def _(request: Request):
-    return auth.login_page(request)
-
-
-@app.post("/login")
-async def _(request: Request):
-    return await auth.login(request)
-
-
-@rt("/logout")
-def _(request: Request):
-    return auth.logout(request)
---
+++ crablox/routes.py
@@ -0,0 +1,52 @@
+from starlette.requests import Request
+from fasthtml.common import Div
+from pathlib import Path
+
+import auth
+import audio
+import blocks
+import hauls
+import index
+import ticker
+
+def register_routes(app, rt):
+    @rt("/")
+    def _():
+        return index.route(rt)
+
+    @rt("/api/blocks/{id}")
+    def _(id: str):
+        for block in hauls.blocks:
+            if block.id == id:
+                return blocks.stack(block)
+        return f"Block not found: {id}", 404
+
+    @rt("/api/history/{parent}/{filename}")
+    def _(parent: str, filename: str):
+        file_path = Path("crablox/hauls") / parent / filename
+        markup = file_path.read_text()
+        return Div(markup, cls="marked")
+
+    @rt("/api/lookup")
+    def _(request: Request):
+        return ticker.display(request)
+
+    @rt("/api/search")
+    def _(request: Request):
+        return ticker.search(request)
+
+    @rt("/audio/{id}")
+    def _(id: str):
+        return audio.get_audio_file(id)
+
+    @app.get("/login")
+    def _(request: Request):
+        return auth.login_page(request)
+
+    @app.post("/login")
+    async def _(request: Request):
+        return await auth.login(request)
+
+    @rt("/logout")
+    def _(request: Request):
+        return auth.logout(request) 
