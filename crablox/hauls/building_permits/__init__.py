f254204abd0e2f9ead4324bd780e58f3355ff823
Steven Almeroth <steven.almeroth@gmail.com>
2025-05-30 09:58:34 -0400

    update bld permits

--- crablox/hauls/building_permits/__init__.py
+++ crablox/hauls/building_permits/__init__.py
@@ -2,11 +2,11 @@ from fasthtml.common import Card, Div, Img, P
 
 from hauls.components import get_details, get_history, get_footer
 
-title = "Bld Permits · 1.47 M"
+title = "Bld Permits · 1.41 M"
 style = "background-color: var(--pico-color-jade-500); border-color: var(--pico-color-jade-300);"
 caption = "United States Building Permits"
-summary = "Building permits in the United States rose by 0.5% to a seasonally adjusted annualized rate of 1.467 million in March 2025, compared to the preliminary estimate of a 1.6% increase, final data showed. This marked the first increase after three consecutive months of declines."
-details_file = "2025-03.md"
+summary = "Building permits in the United States fell by 4% to a seasonally adjusted annualized rate of 1.422 million in April 2025, revised upward from the preliminary estimate of 1.412 million."
+details_file = "2025-04.md"
 
 
 def history():
